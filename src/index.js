e310fa7a6adea74c1b498834e77305e22b6f4b80
nijaskhan <nijaskhan93@gmail.com>
2023-09-06 09:42:52 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -4,14 +4,10 @@ import './index.css';
 import App from './App';
 import { ApiProvider } from '@reduxjs/toolkit/query/react';
 import { productsApi } from './features/apiSlice';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
     <ApiProvider api={productsApi}>
       <App />
     </ApiProvider>
-  </Provider>
 );
