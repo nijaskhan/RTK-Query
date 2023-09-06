8666f32696975230eb65c5f316bb2ee2db4bc5df
nijaskhan <nijaskhan93@gmail.com>
2023-09-06 11:21:45 +0530

    createUser mutation query added

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,32 +1,42 @@
 import React from 'react';
-import { useGetAllProductsQuery, useGetProductQuery } from '../features/apiSlice';
+import { useGetAllProductsQuery, useCreateProductMutation } from '../features/apiSlice';
 
 const ProductDatas = () => {
-    const { data, isLoading, isError } = useGetAllProductsQuery();
-    const { data: user } = useGetProductQuery('1');
+    const { refetch: usersRefetch, data, isLoading, isError } = useGetAllProductsQuery();
+    // const { data: user } = useGetProductQuery('1');
+    const [createProduct] = useCreateProductMutation();
+    const createUser = () => {
+        createProduct({
+            name: 'sivasankaran',
+            email: 'sivasankaran@gmail.com',
+            mobile: 3467982312,
+            password: 'sivasankaran'
+        }).then((res) => {
+            console.log(res, 'response');
+            usersRefetch();
+        })
+    }
 
     return (
         <>
-            <>
-                <h2 style={{textDecoration: 'underline'}}>
-                    List of Users
-                </h2>
-                {
-                    isError ? (
-                        <h3>Oh no, there was an error</h3>
-                    ) : isLoading ? (
-                        <h1>Loading...</h1>
-                    ) : data ? data.map(user => (
-                        <React.Fragment key={user.id}>
-                            <ul>{user.name}</ul>
-                            <li>{user.email}</li>
-                        </React.Fragment>
-                    )) : null
-                }
-            </>
+            <h2 style={{ textDecoration: 'underline' }}>
+                List of Users
+            </h2>
             {
-                user ? <h2>{user.name}</h2> : null
+                isError ? (
+                    <h3>Oh no, there was an error</h3>
+                ) : isLoading ? (
+                    <h1>Loading...</h1>
+                ) : data?.users.length > 0 ? data.users.map(user => (
+                    <React.Fragment key={user._id}>
+                        <ul>{user.name}</ul>
+                        <li>{user.email}</li>
+                    </React.Fragment>
+                )) : null
             }
+            <div style={{ marginBottom: '2rem', marginTop: '2rem' }}>
+                <button onClick={createUser}>create user</button>
+            </div>
         </>
     )
 }
--- src/features/apiSlice.js
+++ src/features/apiSlice.js
@@ -3,16 +3,23 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const productsApi = createApi({
     reducerPath: 'productsApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'https://jsonplaceholder.typicode.com/'
+        baseUrl: 'http://localhost:5000/'
     }),
     endpoints: (builder) => ({
         getAllProducts: builder.query({
-            query: () => '/users'
+            query: () => '/api/admin/get-users'
         }),
         getProduct: builder.query({
             query: (userId) => `/users/${userId}`
+        }),
+        createProduct: builder.mutation({
+            query: (product) => ({
+                url: '/api/users/signup',
+                method: 'POST',
+                body: product
+            })
         })
     })
 });
 
-export const { useGetAllProductsQuery, useGetProductQuery } = productsApi;
+export const { useGetAllProductsQuery, useGetProductQuery, useCreateProductMutation } = productsApi;
